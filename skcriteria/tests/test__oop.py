9bb5152f3e14d45b8c685c094aad7f3896282182
Juan B Cabral <jbc.develop@gmail.com>
2017-04-09 02:32:25 -0300

    i think this gonna work

---
+++ skcriteria/tests/test__oop.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016-2017, Cabral, Juan; Luczywo, Nadia
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+
+# * Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+
+# * Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+# =============================================================================
+# FUTURE
+# =============================================================================
+
+from __future__ import unicode_literals
+
+
+# =============================================================================
+# DOC
+# =============================================================================
+
+__doc__ = """Test io functionalities"""
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+import random
+
+import numpy as np
+
+from .. import _oop
+
+from . import core
+
+
+# =============================================================================
+# BASE
+# =============================================================================
+
+class TestData(core.SKCriteriaTestCase):
+
+    def setUp(self):
+        self.mtx = np.random.rand(3, 3)
+        self.criteria = np.asarray([random.choice((1, -1)) for e in range(3)])
+        self.weights = np.random.randint(1, 100, 3)
+
+    def test_trivial(self):
+        data = _oop.Data(self.mtx, self.criteria, self.weights)
+        other = _oop.Data(self.mtx, self.criteria, self.weights)
+        self.assertEqual(data, other)
+
+    def test_unicode_string(self):
+        cnames = anames = u"ñññ"
+        data = _oop.Data(
+            self.mtx, self.criteria, self.weights,
+            anames=anames, cnames=cnames)
+        data.to_str()
+        data.to_str(tablefmt="html")
+
+        repr(data)
+        str(data)
