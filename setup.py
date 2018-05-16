3c55c41b237d1f3488a7567972a922cf6c343b8d
Juan B Cabral <jbc.develop@gmail.com>
2018-05-15 23:50:59 -0300

    py37

--- setup.py
+++ setup.py
@@ -92,6 +92,8 @@ def do_setup():
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.4",
             "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: Implementation :: CPython",
             "Topic :: Scientific/Engineering",
         ),
