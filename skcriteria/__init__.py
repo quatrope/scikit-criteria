fcf35fb2b319585e69bea45f9a114760290f0ed6
Juan B Cabral <jbc.develop@gmail.com>
2019-12-18 00:04:05 -0300

    ported some things of the tests

---
+++ skcriteria/__init__.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# License: BSD-3 (https://tldrlegal.com/license/bsd-3-clause-license-(revised))
+# Copyright (c) 2016-2019, Cabral, Juan; Luczywo, Nadia
+# All rights reserved.
+
+# =============================================================================
+# DOCS
+# =============================================================================
+
+"""Scikit-Criteria is a collections of algorithms, methods and
+techniques for multiple-criteria decision analysis.
+
+"""
+
+# =============================================================================
+# CONSTANTS
+# =============================================================================
+
+__version__ = ("0", "3")
+
+NAME = "scikit-criteria"
+
+DOC = __doc__
+
+VERSION = ".".join(__version__)
+
+AUTHORS = "Cabral & Luczywo"
+
+EMAIL = "jbc.develop@gmail.com"
+
+URL = "http://scikit-criteria.org/"
+
+LICENSE = "3 Clause BSD"
+
+KEYWORDS = (
+    "mcda mcdm moora electre critiq simus topsis muti criteria"
+    "weightedsum").split()
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+import os
+
+if os.getenv("__SKCRITERIA_IN_SETUP__") != "True":
+    from .data import *  # noqa
+
+del os
