c6bf109b5d16cd4a04f6ea62b50ad2f741d81376
Juan B Cabral <jbc.develop@gmail.com>
2019-12-18 15:58:28 -0300

    __initialization__ now skip frozen=True

--- skcriteria/attribute.py
+++ skcriteria/attribute.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # License: BSD-3 (https://tldrlegal.com/license/bsd-3-clause-license-(revised))
-# Copyright (c) 2016-2017, Cabral, Juan; Luczywo, Nadia
+# Copyright (c) 2016-2019, Cabral, Juan; Luczywo, Nadia
 # All rights reserved.
 
 # =============================================================================
@@ -17,10 +17,32 @@
 # IMPORTS
 # =============================================================================
 
+from unittest import mock
+
 import attr
 
 
+# =============================================================================
+# META
+# =============================================================================
+
 __all__ = ["AttributeClass"]
+
+
+# =============================================================================
+# CONFIGURATION CLASS
+# =============================================================================
+
+@attr.s(frozen=True)
+class AttrCC:
+    configuration = attr.ib()
+    initialization = attr.ib()
+    original_setattr = attr.ib()
+    full_qualname = attr.ib()
+
+    @property
+    def frozen(self):
+        return self.configuration.get("frozen", False)
 
 
 # =============================================================================
@@ -41,13 +63,34 @@ class AttributeMeta(type):
         except NameError:
             return cls
 
-        if not hasattr(cls, "__configuration__"):
-            cls.__configuration__ = {}
+        if hasattr(cls, "__configuration__"):
+            attrs_configuration = dict(cls.__configuration__)
+            del cls.__configuration__
+        else:
+            attrs_configuration = {}
 
-        if not hasattr(cls, "__initialization__"):
-            cls.__initialization__ = attr.NOTHING
+        if hasattr(cls, "__initialization__"):
+            attrs_initialization = cls.__initialization__
+            del cls.__initialization__
+        else:
+            attrs_initialization = attr.NOTHING
 
-        cls = attr.s(**cls.__configuration__)(cls)
+        original_setattr = cls.__setattr__
+
+        # https://stackoverflow.com/a/2020083
+        module = cls.__module__
+        full_qualname = (
+            cls.__name__  # Avoid reporting __builtin__
+            if module is None or module == str.__class__.__module__ else
+            f"{module}.{cls.__name__}")
+
+        cls._attrcc = AttrCC(
+            configuration=attrs_configuration,
+            initialization=attrs_initialization,
+            original_setattr=original_setattr,
+            full_qualname=full_qualname)
+
+        cls = attr.s(**attrs_configuration)(cls)
 
         return cls
 
@@ -66,7 +109,7 @@ class AttributeClass(metaclass=AttributeMeta):
 
     @classmethod
     def parameter(cls, default=attr.NOTHING, validator=None, repr=True,
-                  cmp=True, hash=None, init=True, convert=None, metadata=None,
+                  eq=True, hash=None, init=True, convert=None, metadata=None,
                   type=None, converter=None, factory=None, kw_only=False,
                   instanceof=attr.NOTHING):
         """Create a new attribute for the current class"""
@@ -79,12 +122,18 @@ class AttributeClass(metaclass=AttributeMeta):
                 validator = attr.validators.instance_of(instanceof)
 
         attrib = attr.ib(
-            default=default, validator=validator, repr=repr, cmp=cmp,
-            hash=hash, init=init, convert=convert, metadata=metadata,
-            type=type, converter=converter, factory=factory, kw_only=kw_only)
+            default=default, validator=validator, repr=repr, eq=eq,
+            hash=hash, init=init, metadata=metadata, type=type,
+            converter=converter, factory=factory, kw_only=kw_only)
 
         return attrib
 
     def __attrs_post_init__(self):
-        if self.__initialization__ is not attr.NOTHING:
-            self.__initialization__()
+        if self._attrcc.initialization is not attr.NOTHING:
+            if self._attrcc.frozen:
+                to_patch = f"{self._attrcc.full_qualname}.__setattr__"
+                patch_with = self._attrcc.original_setattr
+                with mock.patch(to_patch, patch_with, create=True):
+                    self._attrcc.initialization(self)
+            else:
+                self._attrcc.initialization(self)
--- skcriteria/data.py
+++ skcriteria/data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # License: BSD-3 (https://tldrlegal.com/license/bsd-3-clause-license-(revised))
-# Copyright (c) 2016-2017, Cabral, Juan; Luczywo, Nadia
+# Copyright (c) 2016-2019, Cabral, Juan; Luczywo, Nadia
 # All rights reserved.
 
 # =============================================================================
@@ -177,4 +177,3 @@ class Data(AttributeClass):
                     f"for {self.mtx.shape[1]} criteria")
 
         self.plot = None
-
