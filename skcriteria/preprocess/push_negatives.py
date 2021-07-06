493ff72a9606760f27672aa3046ee88cad03322f
JuanBC <jbc.develop@gmail.com>
2021-07-06 19:50:37 -0300

    push negatives done

--- skcriteria/preprocess/push_negatives.py
+++ skcriteria/preprocess/push_negatives.py
@@ -8,6 +8,13 @@
 # DOCS
 # =============================================================================
 
+"""Functionalities for remove negatives from criteria.
+
+In addition to the main functionality, an MCDA agnostic function is offered
+to scale data on an array along an arbitrary axis.
+
+"""
+
 # =============================================================================
 # IMPORTS
 # =============================================================================
@@ -25,6 +32,58 @@ from ..utils import doc_inherit
 
 
 def push_negatives(arr: np.ndarray, axis=None) -> np.ndarray:
+    r"""Increment the array until all the valuer are sean >= 0.
+
+    If an array has negative values this function increment the values
+    proportionally to made all the array positive along an axis.
+
+    .. math::
+
+        \overline{X}_{ij} =
+            \begin{cases}
+                X_{ij} + min_{X_{ij}} & \text{if } X_{ij} < 0\\
+                X_{ij}          & \text{otherwise}
+            \end{cases}
+
+    Parameters
+    ----------
+    arr: :py:class:`numpy.ndarray` like.
+        A array with values
+    axis : :py:class:`int` optional
+        Axis along which to operate.  By default, flattened input is used.
+
+    Returns
+    -------
+    :py:class:`numpy.ndarray`
+        array of ratios
+
+    Examples
+    --------
+    .. code-block:: pycon
+
+        >>> from skcriteria.preprocess import push_negatives
+        >>> mtx = [[1, 2], [3, 4]]
+        >>> mtx_lt0 = [[-1, 2], [3, 4]] # has a negative value
+
+        >>> push_negatives(mtx) # array without negatives don't be affected
+        array([[1, 2],
+               [3, 4]])
+
+        # all the array is incremented by 1 to eliminate the negative
+        >>> push_negatives(mtx_lt0)
+        array([[0, 3],
+               [4, 5]])
+
+        # by column only the first one (with the negative value) is affected
+        >>> push_negatives(mtx_lt0, axis=0)
+        array([[0, 2],
+               [4, 4]])
+        # by row only the first row (with the negative value) is affected
+        >>> push_negatives(mtx_lt0, axis=1)
+        array([[0, 3],
+               [3, 4]])
+
+    """
     arr = np.asarray(arr)
     mins = np.min(arr, axis=axis, keepdims=True)
     delta = (mins < 0) * mins
@@ -32,6 +91,22 @@ def push_negatives(arr: np.ndarray, axis=None) -> np.ndarray:
 
 
 class PushNegatives(SKCMatrixAndWeightTransformerMixin, SKCBaseDecisionMaker):
+    r"""Increment the matrix/weights until all the valuer are sean >= 0.
+
+    If the matrix/weights has negative values this function increment the
+    values proportionally to made all the matrix/weights positive along an
+    axis.
+
+    .. math::
+
+        \overline{X}_{ij} =
+            \begin{cases}
+                X_{ij} + min_{X_{ij}} & \text{if } X_{ij} < 0\\
+                X_{ij}          & \text{otherwise}
+            \end{cases}
+
+    """
+
     @doc_inherit(SKCMatrixAndWeightTransformerMixin.transform_weights)
     def transform_weights(self, weights: np.ndarray) -> np.ndarray:
         return push_negatives(weights, axis=None)
