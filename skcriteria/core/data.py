5b77b20d7a65d1c3d0a8284630919d7c1c222fbe
JuanBC <jbc.develop@gmail.com>
2022-02-08 15:50:09 -0300

    Stats splited in a new module

--- skcriteria/core/data.py
+++ skcriteria/core/data.py
@@ -34,6 +34,7 @@ import pyquery as pq
 
 
 from .plot import DecisionMatrixPlotter
+from .stats import DecisionMatrixStatsAccessor
 from ..utils import deprecated, doc_inherit
 
 
@@ -113,105 +114,6 @@ class Objective(enum.Enum):
             return Objective._MIN_STR.value
         if self.value in Objective._MAX_ALIASES.value:
             return Objective._MAX_STR.value
-
-
-# =============================================================================
-# STATS ACCESSOR
-# =============================================================================
-
-
-class DecisionMatrixStatsAccessor:
-    """Calculate basic statistics of the decision matrix."""
-
-    _DF_WHITELIST = (
-        "corr",
-        "cov",
-        "describe",
-        "kurtosis",
-        "mad",
-        "max",
-        "mean",
-        "median",
-        "min",
-        "pct_change",
-        "quantile",
-        "sem",
-        "skew",
-        "std",
-        "var",
-    )
-
-    _DEFAULT_KIND = "describe"
-
-    def __init__(self, dm):
-        self._dm = dm
-
-    def __call__(self, kind=None, **kwargs):
-        """Calculate basic statistics of the decision matrix.
-
-        Parameters
-        ----------
-        kind : str
-            The kind of statistic to produce:
-
-            - 'corr' : Compute pairwise correlation of columns, excluding
-              NA/null values.
-            - 'cov' : Compute pairwise covariance of columns, excluding NA/null
-              values.
-            - 'describe' : Generate descriptive statistics.
-            - 'kurtosis' : Return unbiased kurtosis over requested axis.
-            - 'mad' : Return the mean absolute deviation of the values over the
-              requested axis.
-            - 'max' : Return the maximum of the values over the requested axis.
-            - 'mean' : Return the mean of the values over the requested axis.
-            - 'median' : Return the median of the values over the requested
-              axis.
-            - 'min' : Return the minimum of the values over the requested axis.
-            - 'pct_change' : Percentage change between the current and a prior
-              element.
-            - 'quantile' : Return values at the given quantile over requested
-              axis.
-            - 'sem' : Return unbiased standard error of the mean over requested
-              axis.
-            - 'skew' : Return unbiased skew over requested axis.
-            - 'std' : Return sample standard deviation over requested axis.
-            - 'var' : Return unbiased variance over requested axis.
-
-        **kwargs
-            Options to pass to subjacent DataFrame method.
-
-        Returns
-        -------
-        :class:`matplotlib.axes.Axes` or numpy.ndarray of them
-           The ax used by the plot
-
-        """
-        kind = self._DEFAULT_KIND if kind is None else kind
-
-        if kind.startswith("_"):
-            raise ValueError(f"invalid kind name '{kind}'")
-
-        method = getattr(self, kind, None)
-        if not callable(method):
-            raise ValueError(f"Invalid kind name '{kind}'")
-
-        return method(**kwargs)
-
-    def __repr__(self):
-        """x.__repr__() <==> repr(x)."""
-        return f"{type(self).__name__}({self._dm})"
-
-    def __getattr__(self, a):
-        """x.__getattr__(a) <==> x.a <==> getattr(x, "a")."""
-        if a not in self._DF_WHITELIST:
-            raise AttributeError(a)
-        return getattr(self._dm._data_df, a)
-
-    def __dir__(self):
-        """x.__dir__() <==> dir(x)."""
-        return super().__dir__() + [
-            e for e in dir(self._dm._data_df) if e in self._DF_WHITELIST
-        ]
 
 
 # =============================================================================
---
+++ tests/core/test_stats.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# License: BSD-3 (https://tldrlegal.com/license/bsd-3-clause-license-(revised))
+# Copyright (c) 2016-2021, Cabral, Juan; Luczywo, Nadia
+# Copyright (c) 2022, QuatroPe
+# All rights reserved.
+
+# =============================================================================
+# DOCS
+# =============================================================================
+
+"""test for skcriteria.core.stats
+
+"""
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+import numpy as np
+
+import pandas as pd
+
+
+import pytest
+
+from skcriteria.core import data
+
+
+# =============================================================================
+# STATS
+# =============================================================================
+
+
+def test_DecisionMatrixStatsAccessor_default_kind(decision_matrix):
+    dm = decision_matrix(
+        seed=42,
+        min_alternatives=10,
+        max_alternatives=10,
+        min_criteria=3,
+        max_criteria=3,
+    )
+
+    stats = data.DecisionMatrixStatsAccessor(dm)
+
+    assert stats().equals(dm._data_df.describe())
+
+
+@pytest.mark.parametrize(
+    "kind", data.DecisionMatrixStatsAccessor._DF_WHITELIST
+)
+def test_DecisionMatrixStatsAccessor_df_whitelist_by_kind(
+    kind, decision_matrix
+):
+    dm = decision_matrix(
+        seed=42,
+        min_alternatives=10,
+        max_alternatives=10,
+        min_criteria=3,
+        max_criteria=3,
+    )
+
+    expected = getattr(dm._data_df, kind)()
+
+    stats = data.DecisionMatrixStatsAccessor(dm)
+
+    result_call = stats(kind=kind)
+
+    cmp = (
+        lambda r, e: r.equals(e)
+        if isinstance(result_call, (pd.DataFrame, pd.Series))
+        else np.equal
+    )
+
+    result_method = getattr(stats, kind)()
+
+    assert cmp(result_call, expected)
+    assert cmp(result_method, expected)
+
+
+def test_DecisionMatrixStatsAccessor_invalid_kind(decision_matrix):
+    dm = decision_matrix(
+        seed=42,
+        min_alternatives=10,
+        max_alternatives=10,
+        min_criteria=3,
+        max_criteria=3,
+    )
+
+    stats = data.DecisionMatrixStatsAccessor(dm)
+
+    with pytest.raises(ValueError):
+        stats("_dm")
+
+    stats.foo = None
+    with pytest.raises(ValueError):
+        stats("foo")
+
+    with pytest.raises(AttributeError):
+        stats.to_csv()
+
+
+def test_DecisionMatrixStatsAccessor_repr(decision_matrix):
+    dm = decision_matrix(
+        seed=42,
+        min_alternatives=10,
+        max_alternatives=10,
+        min_criteria=3,
+        max_criteria=3,
+    )
+
+    stats = data.DecisionMatrixStatsAccessor(dm)
+
+    assert repr(stats) == f"DecisionMatrixStatsAccessor({repr(dm)})"
+
+
+def test_DecisionMatrixStatsAccessor_dir(decision_matrix):
+    dm = decision_matrix(
+        seed=42,
+        min_alternatives=10,
+        max_alternatives=10,
+        min_criteria=3,
+        max_criteria=3,
+    )
+
+    stats = data.DecisionMatrixStatsAccessor(dm)
+
+    expected = set(data.DecisionMatrixStatsAccessor._DF_WHITELIST)
+    result = dir(stats)
+
+    assert not expected.difference(result)
