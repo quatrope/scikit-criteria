c8b1b6b5be0138b4f2efdf1d20bdc9d4ec393df3
irmrz <ignaciotramirez02@gmail.com>
2025-06-13 20:59:35 +0000

    metodo edas agregado

--- skcriteria/agg/edas.py
+++ skcriteria/agg/edas.py
@@ -2,23 +2,21 @@
 # -*- coding: utf-8 -*-
 # License: BSD-3 (https://tldrlegal.com/license/bsd-3-clause-license-(revised))
 # Copyright (c) 2016-2021, Cabral, Juan; Luczywo, Nadia
-# Copyright (c) 2022-2025 QuatroPe
+# Copyright (c) 2022, 2023, 2024 QuatroPe
 # All rights reserved.
 
 # =============================================================================
 # DOCS
 # =============================================================================
+""" Evaluatio based on Distance from Average Solution (EDAS) is introduced for 
+multi-criteria inventory classification (MCIC) problems. In the proposed method, 
+we use positive and negative distances fromthe average solution for appraising 
+alternatives.
 
-""" The proposed method that is called EDAS (Evaluation based
-on Distance from Average Solution) uses average solution for appraising the alternatives
-(inventory items). Two measures which called PDA (positive distance from average) and
-NDA (negative distance from average) are considered for the appraisal in this study. These
-measures are calculated according to the type of criteria (beneficial or non-beneficial).
-
-This method is very useful when we have some conflicting criteria.
-The desirable alternative has lower distance from ideal solution and higher distance 
-from nadir solution in these MCDM methods. However, the best alternative in the 
-proposed method is related to the distance from average solution (AV )."""
+Although the proposed method is used for ABC classification of inventory items, 
+this method can also be used for MCDM problems. The best alternative in the 
+proposed method is related to the distance from average solution (AV). 
+"""
 
 # =============================================================================
 # IMPORTS
@@ -27,46 +25,74 @@ proposed method is related to the distance from average solution (AV )."""
 from ..utils import hidden
 
 with hidden():
+    import warnings
+
     import numpy as np
 
     from ._agg_base import RankResult, SKCDecisionMakerABC
     from ..core import Objective
-    from ..utils import doc_inherit, rank
-
+    from ..preprocessing.scalers import scale_by_sum
+    from ..utils import doc_inherit, lp, rank
 
 # =============================================================================
 # EDAS
 # =============================================================================
 
-def edas(matrix, objectives):
-    decision_matrix = np.array(matrix)
+def edas(matrix, weights, beneficial):
+    """Execute edas without any validation"""
+    """Step 1: Select criteria"""
+    """Step 2: Construct the decision matrix"""
 
-    rows = np.shape(decision_matrix)[0]
-    columns = np.shape(decision_matrix)[1]
+    """Step 3: Determine the average solution for each criteria"""
+    average_solution = np.mean(matrix, axis=0)
 
-    average_solution = np.mean(decision_matrix, axis=0)
+    """Step 4: Calculate the positive (PDA) and distance (NDA) from average"""
+    pda = np.zeros_like(matrix)
+    nda = np.zeros_like(matrix)
 
+    for j in range(matrix.shape[1]):
+        is_beneficial = beneficial[j] == True
+        avg_j = average_solution[j]
+        if is_beneficial:
+            pda[:, j] = np.divide(np.maximum(0, (matrix[:, j] - avg_j)), avg_j)
+            nda[:, j] = np.divide(np.maximum(0, (avg_j - matrix[:, j])), avg_j)
+        else:
+            pda[:, j] = np.divide(np.maximum(0, (avg_j - matrix[:, j])), avg_j)
+            nda[:, j] = np.divide(np.maximum(0, (matrix[:, j] - avg_j)), avg_j)
+    
+    """Step 5: Determine the weighted sum of PDA and NDA for all alternatives"""
+    sum_pda = np.sum(pda * weights, axis=1)
+    sum_nda = np.sum(nda * weights, axis=1)
 
+    """Step 6: Normalize the values of weighted sums for all alternatives"""
+    normalized_sum_pda = np.divide(sum_pda, np.max(sum_pda))
+    normalized_sum_nda = 1 - np.divide(sum_nda, np.max(sum_nda))
 
+    """Step 7: Calculate the appraisal score for all alternatives"""
+    scores = np.multiply(0.5, (normalized_sum_pda + normalized_sum_nda))
 
-class EDAS(SKDecisionMakesABC):
-    r""" Descripcion facha """
+    """Step 8: Rank the alternatives according to the decreasing values of the score"""
+    ranked = np.argsrot(-scores)
+    
+    return rank.rank_values(ranked), ranked 
 
+class EDAS(SKCDecisionMakerABC):
     _skcriteria_parameters = []
 
     @doc_inherit(SKCDecisionMakerABC._evaluate_data)
-    def _evaluate_data(self, matrix, objectives):
-        if np.any(matrix < 0):
-            raise ValueError("Edas can't operate with value < 0")
-        
-        rank, score = edas(matrix, weights)
+    def _evaluate_data(self, matrix, weights, beneficial, **kwargs):
+        if np.any(matrix <= 0):
+            raise ValueError(
+                "Edas can't operate with values <= 0"
+            )
+        rank, score = edas(matrix, weights, beneficial)
         return rank, {"score": score}
-
+    
     @doc_inherit(SKCDecisionMakerABC._make_result)
     def _make_result(self, alternatives, values, extra):
         return RankResult(
-            "WeightedSumModel",
+            "EDAS",
             alternatives=alternatives,
             values=values,
-            extra=extra,
+            extra=extra
         )
