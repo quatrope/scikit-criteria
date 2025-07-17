dc119b1b6d76de7638d9a5005a4a7611fdc84247
JBC <jbcabral@unc.edu.ar>
2025-07-17 15:12:45 -0300

    remove float128 from dmsy test

--- tests/io/test_dmsy.py
+++ tests/io/test_dmsy.py
@@ -64,7 +64,7 @@ def test_CustomYAMLDumper_scalar():
         out = yaml.dump(data, Dumper=skc.io.dmsy.CustomYAMLDumper)
         assert out == "1\n...\n"
 
-    for dtype in [np.float16, np.float32, np.float64, np.float128]:
+    for dtype in [np.float16, np.float32, np.float64]:
         data = dtype(1.0)
         out = yaml.dump(data, Dumper=skc.io.dmsy.CustomYAMLDumper)
         assert out == "1.0\n...\n"
